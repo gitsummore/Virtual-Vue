3b9f964b0430093072aa3a029ead572d8f829496
Justin Pierson <justinpierson@Justins-MacBook-Pro.local>
2017-04-21 15:35:33 -0700

    demo list

---
+++ src/components/Demo.vue
@@ -0,0 +1,20 @@
+<template>
+	<list items="dataArray" width="300" height="300" rowHeight="20" rowRender=""></list>
+</template>
+
+<script>
+import list from './List'
+
+export default {
+	data() {
+		return {
+			dataArray: new Array(1000).fill('This is a long ass string')
+		};
+	},
+	methods: {
+		rowRender: function(index, offset) {
+			return '<div style="position:absolute; top:{{ offset }}">{{ this.dataArray[index] }}</div>'
+		}
+	}
+}
+</script>
